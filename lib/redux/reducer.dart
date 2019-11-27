ab57a794562ffc72f3771380c474bd43fe7c4250
Angga Dwi Arifandi <namapanjanggua@gmail.com>
2019-11-28 01:40:31 +0700

    added navigateToNextAndReplace

--- lib/redux/reducer.dart
+++ lib/redux/reducer.dart
@@ -5,12 +5,19 @@ import 'app_state.dart';
 
 final reducer = combineReducers<AppState>([
   TypedReducer<AppState, NavigateToNext>(_navigateToNextReducer),
+  TypedReducer<AppState, NavigateToNextAndReplace>(_navigateToNextAndReplaceReducer),
   TypedReducer<AppState, NavigateBack>(_navigateBackReducer)
 ]);
 
 AppState _navigateToNextReducer(AppState appState, NavigateToNext action) {
   return appState.copy(
       activePages: appState.activePages..add(action.destination)
+  );
+}
+
+AppState _navigateToNextAndReplaceReducer(AppState appState, NavigateToNextAndReplace action) {
+  return appState.copy(
+      activePages: appState.activePages..removeLast()..add(action.destination)
   );
 }
 
